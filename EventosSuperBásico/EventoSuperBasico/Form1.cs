a5dd10bc7f2fa8c38b8c8fa92b849065b1ece211
demian44 <demi-44@hotmail.com>
2019-06-26 12:26:57 -0300

    Agrego ejemplos más básicos /m/

---
+++ EventosSuperBásico/EventoSuperBasico/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EventoSuperBasico
+{
+    delegate void ElDelegado(string numeroCualquiera);
+    public partial class Form1 : Form
+    {
+        event ElDelegado ElEvento;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Antes de asignar this.ElEvento es null
+            ElEvento += this.MostrarMensajeUno; //Acá this.ElEvento dejo de ser null
+            ElEvento += this.MostrarMensajeDos;
+            ElEvento.Invoke("Mensaje que paso al invoke");
+        }
+
+        void MostrarMensajeUno(string numeritoCualquiera)
+        {
+            // Puede hacer lo que se te cante
+            MessageBox.Show("MostrarMensajeUno: este es un manejador. Mensaje recibido: '" + numeritoCualquiera + "'.");
+        }
+
+
+        void MostrarMensajeDos(string numeritoCualquiera)
+        {
+            // Puede hacer lo que se te cante
+            MessageBox.Show("MostrarMensajeDos: este es otro manejador. Mensaje recibido: '" + numeritoCualquiera + "'.");
+        }
+    }
+}
---
+++ EventosSuperBásico/EventosSuperBásico/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EventosSuperBásico
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Thread hilo = new Thread(this.MuyClaroLisa);
+            Thread hiloDos = new Thread(this.OMuyOscuuuuro);
+
+            hilo.Start(); 
+            hiloDos.Start(); 
+        }
+
+        void MuyClaroLisa()
+        {
+            Thread.Sleep(3000); // Importane el tiempo que duerme el hilo para saber cuanto se demora en mostrar el mensaje
+            MessageBox.Show("Muy claro Lisa?");
+        }
+
+        void OMuyOscuuuuro()
+        {
+            Thread.Sleep(1000);
+            MessageBox.Show("O muy oscuuuuuro.");
+        }
+    }
+}
